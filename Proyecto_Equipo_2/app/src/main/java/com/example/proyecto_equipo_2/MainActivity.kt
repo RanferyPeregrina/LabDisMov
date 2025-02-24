3c80166081ed6423ea067d8ba7eb3abf8221955d
Ranfery <88330135+RanferyPeregrina@users.noreply.github.com>
2025-02-24 15:43:55 -0600

    Botón iniciar sesión
    Añadí el botón para iniciar sesión, pero no lo vinculé con la ID de btnIniciarSesión porque ese botón era para ENTRAR a la sesión en
    loginActivity.kt
    
    Le puse btnIniciarSesion_0 o algo así.

--- Proyecto_Equipo_2/app/src/main/java/com/example/proyecto_equipo_2/MainActivity.kt
+++ Proyecto_Equipo_2/app/src/main/java/com/example/proyecto_equipo_2/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.proyecto_equipo_2
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
@@ -7,12 +8,16 @@ import com.google.android.material.button.MaterialButton
 
 class MainActivity : AppCompatActivity() {
 
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         // Configurar botones
         findViewById<MaterialButton>(R.id.btnIrARegistro).setOnClickListener {
+            startActivity(Intent(this, RegisterActivity::class.java)) // Inicia RegisterActivity
+        }
+        findViewById<MaterialButton>(R.id.btnIniciarSesion_0).setOnClickListener {
             startActivity(Intent(this, RegisterActivity::class.java)) // Inicia RegisterActivity
         }
     }
